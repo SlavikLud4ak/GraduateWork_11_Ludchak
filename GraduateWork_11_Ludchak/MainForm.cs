fce862a52987022e5270510fbbb8d4bca82ddc5c
Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>
2023-04-26 22:24:06 +0300

    Add project files.

---
+++ GraduateWork_11_Ludchak/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraduateWork_11_Ludchak
+{
+    public partial class MainForm : MaterialForm
+    {
+
+        public MainForm()
+        {
+            
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightGreen500, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightGreen200, TextShade.WHITE);
+
+        }
+        
+    }
+}
