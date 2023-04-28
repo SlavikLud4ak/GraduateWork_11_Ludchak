3df2e5304d7ff5dbfaf83f3911bb5447773b933f
Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>
2023-04-28 21:54:25 +0300

    update program to netFramework to 4 point 8 and add EF core and add Cosmos DB and changed languge to C# 8

--- GraduateWork_11_Ludchak/MainForm.Designer.cs
+++ GraduateWork_11_Ludchak/MainForm.Designer.cs
@@ -55,7 +55,7 @@
             this.materialTabControl1.Multiline = true;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(1496, 730);
+            this.materialTabControl1.Size = new System.Drawing.Size(1550, 871);
             this.materialTabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -64,7 +64,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 39);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1488, 687);
+            this.tabPage1.Size = new System.Drawing.Size(1542, 828);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Вхід";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -75,7 +75,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 39);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1470, 640);
+            this.tabPage2.Size = new System.Drawing.Size(1524, 781);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Працівники";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -85,7 +85,7 @@
             this.tabPage3.ImageKey = "check-out.png";
             this.tabPage3.Location = new System.Drawing.Point(4, 39);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1470, 640);
+            this.tabPage3.Size = new System.Drawing.Size(1524, 781);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Дати Відпустки";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -95,7 +95,7 @@
             this.tabPage4.ImageKey = "report.png";
             this.tabPage4.Location = new System.Drawing.Point(4, 39);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1470, 640);
+            this.tabPage4.Size = new System.Drawing.Size(1524, 781);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Звіт";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -105,7 +105,7 @@
             this.tabPage5.ImageKey = "logout.png";
             this.tabPage5.Location = new System.Drawing.Point(4, 39);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(1470, 640);
+            this.tabPage5.Size = new System.Drawing.Size(1524, 781);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Вихід";
             this.tabPage5.UseVisualStyleBackColor = true;
@@ -128,7 +128,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1499, 797);
+            this.ClientSize = new System.Drawing.Size(1553, 938);
             this.Controls.Add(this.materialTabControl1);
             this.DrawerShowIconsWhenHidden = true;
             this.DrawerTabControl = this.materialTabControl1;
---
+++ GraduateWork_11_Ludchak/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace GraduateWork_11_Ludchak.Models
+{
+    internal class Employee
+    {
+        public string? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? HomePhone { get; set; }
+
+        
+    }
+}
